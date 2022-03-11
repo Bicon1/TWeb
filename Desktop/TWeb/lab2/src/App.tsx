828f0e4522911d858b92ba755d776c6657577023
Bicon1 <101419409+Bicon1@users.noreply.github.com>
2022-03-11 20:45:15 +0200

    Update App.tsx

--- Desktop/TWeb/lab2/src/App.tsx
+++ Desktop/TWeb/lab2/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Grosu Victor CR192
         </a>
       </header>
     </div>
